312af8eddf8afa08332245dcb2caa4d346a92d17
Wahed <waheedmohammadi126@gmail.com>
2025-11-23 01:54:08 +0100

    useRef example.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import { useRef } from "react";
+
 const App = () => {
+  const inputRef = useRef(null);
+  const submit = () => {
+    console.log(inputRef.current);
+    console.log(inputRef.current.value);
+    inputRef.current.style.backgroundColor = "gray";
+    inputRef.current.style.color = "blue";
+    inputRef.current.style.blure = inputRef.current.setAttribute(
+      "placeholder",
+      "updated..."
+    );
+  };
   return (
-    <>
-      <h2>Simpe Timer App.</h2>
-    </>
+    <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
+      <h2 className="text-2xl font-bold mb-4">useRef Example</h2>
+
+      <input
+        type="text"
+        placeholder="Type something..."
+        className="w-full p-2 border rounded-lg"
+        ref={inputRef}
+      />
+
+      <button
+        onClick={submit}
+        className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      >
+        Submit
+      </button>
+    </div>
   );
 };
 
